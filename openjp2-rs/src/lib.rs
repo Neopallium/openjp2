465323b682fbadac69305353d5de03e55dbee9ad
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-12-09 01:01:55 +0800

    Add file format detection helpers.

--- openjp2-rs/src/lib.rs
+++ openjp2-rs/src/lib.rs
@@ -32,6 +32,81 @@ pub mod image;
 pub mod openjpeg;
 pub mod stream;
 
+/// Magic bytes for JP2 RFC3745.
+pub const JP2_RFC3745_MAGIC: &'static [u8] = &[
+  0x00, 0x00, 0x00, 0x0c, 0x6a, 0x50, 0x20, 0x20, 0x0d, 0x0a, 0x87, 0x0a,
+];
+pub const JP2_MAGIC: &'static [u8] = &[0x0d, 0x0a, 0x87, 0x0a];
+/// Magic bytes for J2K Codestream.
+pub const J2K_CODESTREAM_MAGIC: &'static [u8] = &[0xff, 0x4f, 0xff, 0x51];
+
+/// Supported Jpeg 2000 formats.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum J2KFormat {
+  JP2,
+  J2K,
+  JPT,
+}
+
+/// Detect Jpeg 2000 format from magic bytes.
+pub fn detect_format(buf: &[u8]) -> Result<J2KFormat, String> {
+  if buf.starts_with(JP2_RFC3745_MAGIC) {
+    Ok(J2KFormat::JP2)
+  } else if buf.starts_with(JP2_MAGIC) {
+    Ok(J2KFormat::JP2)
+  } else if buf.starts_with(J2K_CODESTREAM_MAGIC) {
+    Ok(J2KFormat::J2K)
+  } else {
+    Err("Can't detect image format from bytes".into())
+  }
+}
+
+/// Detect Jpeg 2000 format from file extension.
+pub fn detect_format_from_extension(ext: Option<&std::ffi::OsStr>) -> Result<J2KFormat, String> {
+  let lower_ext = ext.and_then(|e| e.to_str()).map(|e| e.to_ascii_lowercase());
+  match lower_ext.as_ref().map(|s| s.as_str()) {
+    Some("jp2") => Ok(J2KFormat::JP2),
+    Some("jpt") => Ok(J2KFormat::JPT),
+    Some("j2k") | Some("j2c") | Some("jpc") => Ok(J2KFormat::J2K),
+    // HTJ2K with JP2 boxes
+    Some("jph") => Ok(J2KFormat::JP2),
+    // HTJ2K codestream
+    Some("jhc") => Ok(J2KFormat::J2K),
+    Some(ext) => Err(format!("Unknown file extension: {}", ext)),
+    None => Err("No file extension".into()),
+  }
+}
+
+#[cfg(feature = "file-io")]
+pub fn detect_format_from_file<P: AsRef<std::path::Path>>(path: P) -> Result<J2KFormat, String> {
+  use alloc::io::Read;
+
+  let path = path.as_ref();
+  let ext = path.extension();
+  let ext_format = detect_format_from_extension(ext)?;
+  if ext_format == J2KFormat::JPT {
+    return Ok(J2KFormat::JPT);
+  }
+
+  // Read magic bytes from file
+  let mut buf = [0; 12];
+  let mut file = std::fs::File::open(path).map_err(|e| e.to_string())?;
+  file.read_exact(&mut buf).map_err(|e| e.to_string())?;
+
+  // Detect format from magic bytes
+  let magic_format = detect_format(&buf)?;
+
+  // Log warning if magic bytes and file extension don't match
+  if ext_format != magic_format {
+    eprintln!(
+      "Warning: File extension format ({:?}) doesn't match magic bytes format ({:?})",
+      ext_format, magic_format
+    );
+  }
+
+  Ok(magic_format)
+}
+
 mod bio;
 mod cio;
 mod codec;
